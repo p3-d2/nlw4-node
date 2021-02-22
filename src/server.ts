d8f5efd80d343fac3f7b5a8131d2559c23a65693
Pedro Henrique <pedro.contato.email@gmail.com>
2021-02-22 09:04:01 -0300

    first class final code

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  return res.json({ message: "Hello World" });
+});
+
+app.post("/", (req, res) => {
+  return res.json({ message: "Os dados foram salvos" });
+});
+
 app.listen(3333, () => {
   console.log("server is running");
 });
