578cb9eb54b1e182c1c5f0aa5d08912049ded6f0
Pedro Henrique <pedro.contato.email@gmail.com>
2021-02-23 09:20:56 -0300

    restructuring the project

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from "express";
+
+class UserController {
+  async create(req: Request, res: Response) {
+    const body = req.body;
+    console.log(body);
+    return res.send();
+  }
+}
+
+export { UserController };
--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,12 @@ import "reflect-metadata";
 import "./database";
 import express from "express";
 
+import { router } from "./routes";
+
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
-});
-
-app.post("/", (req, res) => {
-  return res.json({ message: "Os dados foram salvos" });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => {
   console.log("server is running");
