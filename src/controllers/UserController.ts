4040df66bbf179207d8a82a72c4b3581c134769e
Pedro Henrique <pedro.contato.email@gmail.com>
2021-02-23 09:32:25 -0300

    end of class 2

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,32 @@
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+
+import { User } from "../models/User";
 
 class UserController {
   async create(req: Request, res: Response) {
-    const body = req.body;
-    console.log(body);
-    return res.send();
+    const { name, email } = req.body;
+
+    const userRepository = getRepository(User);
+
+    const userAlreadyExists = await userRepository.findOne({
+      email,
+    });
+
+    if (userAlreadyExists) {
+      return res.status(400).json({
+        error: "user already exists!",
+      });
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+    });
+
+    await userRepository.save(user);
+
+    return res.json(user);
   }
 }
 
