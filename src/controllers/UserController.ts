471b4de65ea09347ff8803ad2b2cd76e130a4951
Pedro Henrique <pedro.contato.email@gmail.com>
2021-02-24 08:14:17 -0300

    refactoring UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,12 @@
 import { Request, Response } from "express";
-import { getRepository } from "typeorm";
-
-import { User } from "../models/User";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 class UserController {
   async create(req: Request, res: Response) {
     const { name, email } = req.body;
 
-    const userRepository = getRepository(User);
+    const userRepository = getCustomRepository(UsersRepository);
 
     const userAlreadyExists = await userRepository.findOne({
       email,
