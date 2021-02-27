b506c0d747e16ced554b543c72e368c00c0c74d6
Pedro Henrique <pedro.contato.email@gmail.com>
2021-02-27 11:25:46 -0300

    creating AnswerController

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,14 @@ import { Router } from "express";
 import { SendMailController } from "./controllers/SendMailController";
 import { SurveyController } from "./controllers/SurveyController";
 import { UserController } from "./controllers/UserController";
+import { AnswerController } from "./controllers/AnswerController";
 
 const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveyController();
 const sendMailController = new SendMailController();
+const answerController = new AnswerController();
 
 router.post("/users", userController.create);
 
@@ -16,5 +18,7 @@ router.post("/surveys", surveyController.create);
 router.get("/surveys", surveyController.show);
 
 router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answerController.execute);
 
 export { router };
