00437a4c65ed7abbb0c54776b0662eb57afb7416
Pedro Henrique <pedro.contato.email@gmail.com>
2021-02-27 12:18:16 -0300

    adding validation in Us UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,19 @@
 import { Request, Response } from "express";
 import { getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
+import * as yup from "yup";
 
 class UserController {
   async create(req: Request, res: Response) {
     const { name, email } = req.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required(),
+      email: yup.string().email().required(),
+    });
+
+    if (!(await schema.isValid(req.body)))
+      return res.status(400).json({ error: "Validation Failed!" });
 
     const userRepository = getCustomRepository(UsersRepository);
 
